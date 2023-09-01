9a5ba9d6bdf598f4592427e2ad8b62dcf7ca52cb
kevincorges <141889047+kevincorges@users.noreply.github.com>
2023-09-01 16:18:40 -0300

    feat: adicionando  classes do model

---
+++ src/main/java/com/uefs/librarymanager/model/Reserva.java
@@ -0,0 +1,9 @@
+package main.java.com.uefs.librarymanager.model;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Reserva {
+    private String livroISBN;
+    private Queue<String> fila;
+}
