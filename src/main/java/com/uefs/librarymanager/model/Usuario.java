1d622dc863a9d64c8ee5ed335ab9fe0421b61a0a
valmir filho <valmirnogueira514@gmail.com>
2023-09-01 15:11:34 -0300

    feat: add classe abstrata Usuario

---
+++ src/main/java/com/uefs/librarymanager/model/Usuario.java
@@ -0,0 +1,10 @@
+package main.java.com.uefs.librarymanager.model;
+
+public abstract class Usuario {
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String id;
+    private String senha;
+
+}
