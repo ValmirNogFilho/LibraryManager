0765103c1b15a6e0743a007fa57f3e7c08caefb1
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-09-09 21:01:27 -0300

    feat: add novas exceptions

---
+++ src/main/java/com/uefs/librarymanager/exceptions/LivroException.java
@@ -0,0 +1,8 @@
+package main.java.com.uefs.librarymanager.exceptions;
+
+public class LivroException extends Exception{
+
+    public LivroException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/uefs/librarymanager/exceptions/UsuarioException.java
@@ -0,0 +1,7 @@
+package main.java.com.uefs.librarymanager.exceptions;
+
+public class UsuarioException extends Exception{
+    public UsuarioException(String message) {
+        super(message);
+    }
+}
