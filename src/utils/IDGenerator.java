e1155d8d62d644fe60e66dea1cf7adbaaec883a7
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-09-17 00:19:07 -0300

    :goal_net: fix: add verificação de não preexistência de id gerado

--- src/utils/IDGenerator.java
+++ src/utils/IDGenerator.java
@@ -2,18 +2,27 @@ package utils;
 
 import java.time.Year;
 import java.util.Calendar;
+import java.util.LinkedList;
 import java.util.Random;
 import java.util.UUID;
 
 import static java.util.Calendar.YEAR;
 
 public class IDGenerator {
+    public static LinkedList<String> IDsRegistrados = new LinkedList<String>();
     public static String geraID() {
         Calendar c = Calendar.getInstance();
         int ano = c.get(Calendar.YEAR);
         ano %=  100;
         Random random = new Random();
-        Integer randint = 1000 + random.nextInt(9999);
-        return ano + randint.toString();
+        Integer randint;
+        String id;
+        do{
+            randint = 1000 + random.nextInt(9999);
+            id = ano + randint.toString();
+        }
+        while(IDsRegistrados.contains(id));
+        IDsRegistrados.add(id);
+        return id;
     }
 }
