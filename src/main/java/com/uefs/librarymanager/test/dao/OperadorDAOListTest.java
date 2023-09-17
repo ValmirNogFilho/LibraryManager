a308e283ac26ea7d935f19e94a7b83ae4f922421
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-09-17 00:45:21 -0300

    :bug: fix: consertando imports

--- src/main/java/com/uefs/librarymanager/test/dao/OperadorDAOListTest.java
+++ src/main/java/com/uefs/librarymanager/test/dao/OperadorDAOListTest.java
@@ -6,9 +6,8 @@ import main.java.com.uefs.librarymanager.model.Usuario;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class OperadorDAOListTest {
 
