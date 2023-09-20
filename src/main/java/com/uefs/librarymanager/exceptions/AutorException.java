fa67640b25642af57c4bd94b645ee7920ada35df
valmir filho <valmirnogueira514@gmail.com>
2023-09-20 13:54:49 -0300

    :fire: removendo exceções de categoria e autor e LivroDAOTest repetido

--- src/main/java/com/uefs/librarymanager/exceptions/AutorException.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.uefs.librarymanager.exceptions;
-
-public class AutorException extends Exception{
-    public AutorException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/uefs/librarymanager/exceptions/CategoriaException.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.uefs.librarymanager.exceptions;
-
-public class CategoriaException extends Exception{
-    public CategoriaException(String message) {
-        super(message);
-    }
-}
