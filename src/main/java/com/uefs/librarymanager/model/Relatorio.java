ad31978b02052c2ef87d957f74cf6aadef457dae
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-09-25 16:50:54 -0300

    :sparkles: feat: add busca de livros mais populares

--- src/main/java/com/uefs/librarymanager/model/Relatorio.java
+++ src/main/java/com/uefs/librarymanager/model/Relatorio.java
@@ -4,10 +4,7 @@ import main.java.com.uefs.librarymanager.dao.DAO;
 import main.java.com.uefs.librarymanager.exceptions.UsuarioException;
 import utils.statusEmprestimo;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Relatorio {
     public int numLivrosEmprestados(){
@@ -33,7 +30,14 @@ public class Relatorio {
     public List<Emprestimo> historicoEmprestimo(String idUsuario) throws UsuarioException {
         return DAO.getEmprestimoDAO().findByLeitor(DAO.getLeitorDAO().findById(idUsuario));
     }
-    public List<Livro> livrosMaisPopulares(){
-        return new ArrayList<Livro>();
+    public List<Livro> livrosMaisPopulares(int primeiros){
+        List<Livro> livros = DAO.getLivroDAO().findMany();
+        livros.sort(Comparator.comparingInt(Livro::getDisponiveis));
+        List<Livro> maisPopulares = new LinkedList<Livro>();
+
+        for(int i = 0; i < Math.min(livros.size(), primeiros); i++)
+            maisPopulares.add(livros.get(i));
+
+        return maisPopulares;
     }
 }
