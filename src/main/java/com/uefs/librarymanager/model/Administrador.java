d9952b9661cf719ee12a616af29165fa2af35de3
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-09-25 22:37:34 -0300

    :sparkles: feat: add desbloquearLeitor

--- src/main/java/com/uefs/librarymanager/model/Administrador.java
+++ src/main/java/com/uefs/librarymanager/model/Administrador.java
@@ -1,31 +1,31 @@
 package main.java.com.uefs.librarymanager.model;
 
 import main.java.com.uefs.librarymanager.dao.DAO;
-import main.java.com.uefs.librarymanager.exceptions.SenhaInvalidaException;
+import main.java.com.uefs.librarymanager.exceptions.UsuarioException;
 import utils.statusLeitor;
 
 public class Administrador extends Usuario {
 
     private static Relatorio relatorio = new Relatorio();
-    public Administrador(String nome, String endereco, String telefone, String senha) {
-        super(nome, endereco, telefone, senha);
+    public Administrador(String nome, String endereco, String telefone) {
+        super(nome, endereco, telefone, null);
     }
 
 
-    public Leitor cadastrarLeitor(String nome, String endereco, String telefone, String senha) throws SenhaInvalidaException {
+    public Leitor cadastrarLeitor(String nome, String endereco, String telefone, String senha) throws UsuarioException {
         Leitor l = new Leitor(nome, endereco, telefone);
         l.setSenha(senha);
         return DAO.getLeitorDAO().create(l);
     }
 
-    public Usuario cadastrarOperador(String nome, String endereco, String telefone, String senha, String ocupacao) throws SenhaInvalidaException{
+    public Usuario cadastrarOperador(String nome, String endereco, String telefone, String senha, String ocupacao) throws UsuarioException{
         Usuario obj = null;
         switch (ocupacao){
             case "Administrador":
-                obj = new Administrador(nome, endereco, telefone, null);
+                obj = new Administrador(nome, endereco, telefone);
                 break;
             case "Bibliotecário":
-                obj = new Bibliotecario(nome, endereco, telefone, null);
+                obj = new Bibliotecario(nome, endereco, telefone);
                 break;
         }
         obj.setSenha(senha);
@@ -34,6 +34,17 @@ public class Administrador extends Usuario {
 
     public void bloquearLeitor(Leitor l) {
         l.setStatus(statusLeitor.BLOQUEADO);
+        DAO.getLeitorDAO().update(l);
+    }
+
+    public void desbloquearLeitor(Leitor l){
+        l.setStatus(statusLeitor.LIVRE);
+        for (Emprestimo emprestimo: DAO.getEmprestimoDAO().findByLeitor(l)){
+
+            Sistema.verificarPossivelMulta(emprestimo, l);
+            DAO.getEmprestimoDAO().update(emprestimo);
+
+        }
         DAO.getLeitorDAO().update(l);
     }
 
