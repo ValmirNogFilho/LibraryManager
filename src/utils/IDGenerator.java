869540f97a719d1ff6858627fe4e7d7b24892932
kevincorges <141889047+kevincorges@users.noreply.github.com>
2023-09-29 16:08:38 -0300

    :pencil2: docs: documentando os utils IDGenerator.java, statusEmprestimo.java e statusLeitor.java

--- src/utils/IDGenerator.java
+++ src/utils/IDGenerator.java
@@ -8,6 +8,18 @@ import java.util.UUID;
 
 import static java.util.Calendar.YEAR;
 
+/**
+ * Esta classe é responsável por gerar o ID de cada usuário da biblioteca. O ID é formado por seis dígitos, sendo dois
+ * os dois últimos números do ano, exemplo: 2023, o ID vai conter o número 23 + quatro números aleatórios.
+ * Exemplo de um possível ID: 232113.
+ * @author  Valmir Alves Nogueira Filho
+ * @author Kevin Cordeiro Borges
+ * @see java.time.Year
+ * @see java.util.Calendar
+ * @see java.util.LinkedList
+ * @see java.util.Random
+ * @see java.util.UUID
+ */
 public class IDGenerator {
     public static LinkedList<String> IDsRegistrados = new LinkedList<String>();
     public static String geraID() {
