5d1fbe7b641bed853580da5e7d41eb54a011543d
kevincorges <141889047+kevincorges@users.noreply.github.com>
2023-09-29 15:25:05 -0300

    :fire: refactor: removendo imports desnecessários. :pencil2: docs: documentando o model Convidado.

--- src/main/java/com/uefs/librarymanager/model/Convidado.java
+++ src/main/java/com/uefs/librarymanager/model/Convidado.java
@@ -3,6 +3,15 @@ package main.java.com.uefs.librarymanager.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Esta classe é referente ao convidado, uma pessoa que não tem conta na biblioteca, mas pode fazer consultas no sistema.
+ * A classe é vazia por conta de o convidado não ter necessidade especificidade alguma, já que ele só pode fazeer pesquisisas
+ * no sistema.
+ * @author Valmir Alves Nogueira Filho
+ * @author Kevin Cordeiro Borges
+ * @see java.util.ArrayList
+ * @see java.util.List
+ */
 public class Convidado {
 
 }
