10c8fe4a60c783fd4ecec7da370b55955fd7f007
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-10-30 17:29:00 -0300

    :sparkles: feat: add classe abstrata para geração de arquivos

---
+++ src/utils/FileBehaviour.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+
+public abstract class FileBehaviour {
+    public static final File DIRETORIO = new File("cache");
+    private static final String EXTENSAO = "kv";
+
+    public static File gerarArquivo(String nomearquivo) {
+
+        if (!DIRETORIO.exists())
+            DIRETORIO.mkdir();
+
+        File arquivo = new File(DIRETORIO.getName() + "/" + nomearquivo + EXTENSAO);
+
+        if (!arquivo.exists())
+            try{
+                arquivo.createNewFile();
+            } catch(Exception e){
+                arquivo = null;
+            }
+
+        return arquivo;
+    }
+
+}
