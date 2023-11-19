5c2e8b929feb069dfbcddb802af344ff2a37c75f
kevincorges <141889047+kevincorges@users.noreply.github.com>
2023-11-19 20:26:39 -0300

    :books: feat Criação do LeitorDAOFile

--- src/main/java/com/uefs/librarymanager/dao/usuario/leitor/LeitorDAOFile.java
+++ src/main/java/com/uefs/librarymanager/dao/usuario/leitor/LeitorDAOFile.java
@@ -2,10 +2,14 @@ package main.java.com.uefs.librarymanager.dao.usuario.leitor;
 
 import main.java.com.uefs.librarymanager.exceptions.UsuarioException;
 import main.java.com.uefs.librarymanager.model.Leitor;
+import main.java.com.uefs.librarymanager.model.Usuario;
 import utils.FileBehaviour;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LeitorDAOFile implements LeitorDAO{
 
@@ -15,38 +19,84 @@ public class LeitorDAOFile implements LeitorDAO{
         arquivo = FileBehaviour.gerarArquivo("leitores");
     }
 
+
     @Override
     public Leitor create(Leitor obj) {
-        return null;
+        Map<String, Leitor> leitores = findManyMap();
+        leitores.put(obj.getId(),obj);
+        deleteMany();
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(arquivo))) {
+            out.writeObject(leitores);
+        } catch (IOException e) {
+            obj = null;
+        }
+        return obj;
+
     }
+
 
     @Override
     public void delete(Leitor obj) {
+        Map<String, Leitor> leitores = findManyMap();
+        leitores.remove(obj.getId());
+        deleteMany();
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(arquivo))) {
+            out.writeObject(leitores);
+        } catch (IOException e) {}
+
 
     }
 
     @Override
     public void deleteMany() {
+        try {
+            new FileOutputStream(arquivo).close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public Leitor update(Leitor obj) {
-        return null;
+        Map<String, Leitor> leitores = findManyMap();
+        leitores.remove(obj.getId());
+        return create(obj);
+
     }
 
     @Override
     public List<Leitor> findMany() {
-        return null;
+        return new ArrayList<Leitor>(findManyMap().values());
     }
 
     @Override
     public Leitor findByPrimaryKey(String PrimaryKey) {
-        return null;
+        return findManyMap().get(PrimaryKey);
     }
 
     @Override
     public Leitor findById(String id) throws UsuarioException {
-        return null;
+        Leitor o = findByPrimaryKey(id);
+        if (o == null)
+            throw new UsuarioException(UsuarioException.NAO_EXISTENTE);
+        else
+            return o;
+    }
+
+    private Map<String, Leitor> findManyMap() {
+        Map<String, Leitor> leitores;
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(arquivo))) {
+            leitores = (Map<String, Leitor>) in.readObject();
+
+        } catch (IOException e){
+            leitores = new HashMap<>();
+        }
+        catch (ClassNotFoundException e) {
+            leitores = new HashMap<>();
+        }
+        return leitores;
     }
 }
