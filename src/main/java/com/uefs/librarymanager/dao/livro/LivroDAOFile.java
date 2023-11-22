697d7f566ebe82483a4b4bfc0aa1dba6a918984e
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-11-22 15:35:45 -0300

    :recycle: refactor: removendo métodos desnecessários e refatorando buscas com filter

--- src/main/java/com/uefs/librarymanager/dao/livro/LivroDAOFile.java
+++ src/main/java/com/uefs/librarymanager/dao/livro/LivroDAOFile.java
@@ -1,194 +1,117 @@
 package main.java.com.uefs.librarymanager.dao.livro;
 
-import main.java.com.uefs.librarymanager.model.Usuario;
-import utils.FileBehaviour;
 import main.java.com.uefs.librarymanager.exceptions.LivroException;
 import main.java.com.uefs.librarymanager.model.Livro;
+import utils.FileBehaviour;
 
-import java.io.*;
+import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class LivroDAOFile implements LivroDAO{
 
     File arquivo;
-    File arquivoCategorias;
-    File arquivoAutores;
+   
 
+    private static final String NOMEARQUIVOLIVROS = "livros";
+  
     public LivroDAOFile(){
-        arquivo = FileBehaviour.gerarArquivo("livros");
-        arquivoCategorias = FileBehaviour.gerarArquivo("categorias");
-        arquivoAutores = FileBehaviour.gerarArquivo("autores");
+        arquivo = FileBehaviour.gerarArquivo(NOMEARQUIVOLIVROS);
+    
     }
 
     @Override
     public Livro create(Livro obj) {
-        Map<String, Livro> livros = findManyLivros();
+        Map<String, Livro> livros = findManyMap();
         livros.put(obj.getISBN(), obj);
-
-        replaceInFile(arquivo, livros);
+        FileBehaviour.sobreescreverArquivo(arquivo, livros);
 
         return obj;
     }
 
     @Override
     public void delete(Livro obj) {
-        Map<String, Livro> livros = findManyLivros();
+        Map<String, Livro> livros = findManyMap();
         livros.remove(obj.getISBN());
 
-        Map <String, LinkedList<String>> isbnPorCategorias = findManyISBNCategorias();
-        LinkedList<String> exemplares = isbnPorCategorias.get(obj.getCategoria());
-
-        Map <String, LinkedList<String>> isbnPorAutores = findManyISBNAutores();
-
-        if(exemplares != null){
-            exemplares.remove(obj);
-            isbnPorCategorias.put(obj.getCategoria(), exemplares);
-        }
-
-        exemplares = isbnPorAutores.get(obj.getCategoria());
-        if(exemplares != null){
-            exemplares.remove(obj);
-            isbnPorCategorias.put(obj.getAutor(), exemplares);
-        }
-
-        replaceInFile(arquivo, livros);
-        replaceInFile(arquivoCategorias, isbnPorCategorias);
-        replaceInFile(arquivoAutores, isbnPorAutores);
+        FileBehaviour.sobreescreverArquivo(arquivo, livros);
 
     }
 
 
     @Override
     public void deleteMany() {
-        try {
-            new FileOutputStream(arquivo).close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        FileBehaviour.apagarConteudoArquivo(arquivo);
     }
 
     @Override
     public Livro update(Livro obj) {
-        Map<String, Livro> livros = findManyLivros();
+        Map<String, Livro> livros = findManyMap();
 
         livros.remove(obj.getISBN());
         livros.put(obj.getISBN(), obj);
 
-        replaceInFile(arquivo, livros);
+        FileBehaviour.sobreescreverArquivo(arquivo, livros);
 
         return obj;
     }
 
     @Override
     public List<Livro> findMany() {
-        Collection<Livro> values = findManyLivros().values();
+        Collection<Livro> values = findManyMap().values();
         return new LinkedList<Livro>(values);
     }
 
     @Override
     public Livro findByPrimaryKey(String ISBN) {
-        return findManyLivros().get(ISBN);
+        return findManyMap().get(ISBN);
     }
-
-    @Override
-    public void addCategoria(String categoria) {
-        LinkedList<String> ISBNs = new LinkedList<String>();
-        Map<String, LinkedList<String>> isbnPorCategorias = findManyISBNCategorias();
-        isbnPorCategorias.put(categoria, ISBNs);
-        replaceInFile(arquivoCategorias, isbnPorCategorias);
-    }
-
-    @Override
-    public Livro addLivroEmCategoria(Livro obj) {
-        String categoria = obj.getCategoria();
-        Map<String, LinkedList<String>> isbnPorCategorias = findManyISBNCategorias();
-
-        if(isbnPorCategorias.get(categoria) == null)
-            addCategoria(categoria);
-
-        isbnPorCategorias.get(categoria).add(obj.getISBN());
-        replaceInFile(arquivoCategorias, isbnPorCategorias);
-        return obj;
-    }
-
-    @Override
-    public void removerLivroDeCategoria(Livro obj, String categoria) {
-        Map<String, LinkedList<String>> isbnPorCategorias = findManyISBNCategorias();
-        LinkedList<String> isbns = isbnPorCategorias.get(categoria);
-        isbns.remove(obj.getISBN());
-        isbnPorCategorias.put(categoria, isbns);
-
-        replaceInFile(arquivoCategorias, isbnPorCategorias);
-    }
-
+    
     @Override
     public List<Livro> findByCategoria(String categoria) {
-        LinkedList<String> isbns = findManyISBNCategorias().get(categoria);
-        List<Livro> livrosDaCategoria = new LinkedList<Livro>();
 
-        Map<String, Livro> livros = findManyLivros();
-        for(String isbn: isbns){
-            Livro livro = livros.get(isbn);
-            livrosDaCategoria.add(livro);
-        }
+        return findMany()
+                .stream()
+                .filter(l -> l.getCategoria().equals(categoria))
+                .collect(Collectors.toList());
 
-        return livrosDaCategoria;
-    }
+//        ArrayList<Livro> livros = new ArrayList<>();
+//
+//        for (String isbn: findManyMap().keySet()) {
+//            Livro livro = findByPrimaryKey(isbn);
+//
+//            if(livro.getCategoria().equals(categoria))
+//                livros.add(livro);
+//        }
+//
+//        return livros;
 
-    @Override
-    public void addAutor(String autor) {
-        LinkedList<String> ISBNs = new LinkedList<String>();
-        Map<String, LinkedList<String>> isbnPorAutores = findManyISBNAutores();
-        isbnPorAutores.put(autor, ISBNs);
-        replaceInFile(arquivoAutores, isbnPorAutores);
     }
 
     @Override
     public List<Livro> findByAutor(String autor) {
-        LinkedList<String> isbns = findManyISBNAutores().get(autor);
-        List<Livro> livrosDoAutor = new LinkedList<Livro>();
-
-        Map<String, Livro> livros = findManyLivros();
-        for(String isbn: isbns){
-            Livro livro = livros.get(isbn);
-            livrosDoAutor.add(livro);
-        }
-
-        return livrosDoAutor;
+        return findMany()
+                .stream()
+                .filter(l -> l.getAutor().equals(autor))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Livro> findByTitulo(String titulo) {
-        List<Livro> livrosTitulo = new LinkedList<Livro>();
-        for(Livro livro: findMany())
-            if(livro.getTitulo().toLowerCase().contains(titulo.toLowerCase()))
-                livrosTitulo.add(livro);
-        return livrosTitulo;
+
+        return findMany()
+                .stream()
+                .filter(l -> l.getTitulo().toLowerCase().contains(titulo.toLowerCase()))
+                .collect(Collectors.toList());
+//
+//        List<Livro> livrosTitulo = new LinkedList<Livro>();
+//        for(Livro livro: findMany())
+//            if(livro.getTitulo().toLowerCase().contains(titulo.toLowerCase()))
+//                livrosTitulo.add(livro);
+//        return livrosTitulo;
     }
 
-    @Override
-    public Livro addLivroEmAutor(Livro obj) {
-        String autor = obj.getAutor();
-        Map<String, LinkedList<String>> isbnPorAutores = findManyISBNAutores();
-
-        if(isbnPorAutores.get(autor) == null)
-            addAutor(autor);
-
-        isbnPorAutores.get(autor).add(obj.getISBN());
-        replaceInFile(arquivoAutores, isbnPorAutores);
-        return obj;
-    }
-
-    @Override
-    public void removerLivroDeAutor(Livro obj, String autor) {
-        Map<String, LinkedList<String>> isbnPorAutores = findManyISBNAutores();
-        LinkedList<String> isbns = isbnPorAutores.get(autor);
-        isbns.remove(obj.getISBN());
-        isbnPorAutores.put(autor, isbns);
-
-        replaceInFile(arquivoAutores, isbnPorAutores);
-    }
-
+   
     @Override
     public Livro findByISBN(String ISBN) throws LivroException {
         Livro l = findByPrimaryKey(ISBN);
@@ -197,45 +120,9 @@ public class LivroDAOFile implements LivroDAO{
         throw new LivroException(LivroException.NAO_EXISTENTE);
     }
 
-
-    private <K,V> Map<K, V> findManyMap(File file){
-        Map<K, V> map;
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
-            map = (Map<K, V>) in.readObject();
-
-        } catch (IOException e){
-            map = new HashMap<>();
-        }
-        catch (ClassNotFoundException e) {
-            map = new HashMap<>();
-        }
-        return map;
+    private Map<String, Livro> findManyMap(){
+        return FileBehaviour.consultarArquivo(arquivo);
     }
-
-    private Map<String, Livro> findManyLivros(){
-        return findManyMap(arquivo);
-    }
-
-
-    private Map<String, LinkedList<String>> findManyISBNAutores() {
-        return findManyMap(arquivoAutores);
-    }
-
-    private Map<String, LinkedList<String>> findManyISBNCategorias() {
-        return findManyMap(arquivoCategorias);
-    }
-
-
-    private <K, V> boolean replaceInFile(File file, Map<K, V> data){
-        deleteMany();
-
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(arquivo))) {
-            out.writeObject(data);
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
+    
 
 }
--- src/main/java/com/uefs/librarymanager/dao/livro/LivroDAOList.java
+++ src/main/java/com/uefs/librarymanager/dao/livro/LivroDAOList.java
@@ -4,6 +4,7 @@ import main.java.com.uefs.librarymanager.exceptions.LivroException;
 import main.java.com.uefs.librarymanager.model.Livro;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class LivroDAOList implements LivroDAO{
     Map<String, Livro> livros;
@@ -18,8 +19,6 @@ public class LivroDAOList implements LivroDAO{
     @Override
     public Livro create(Livro obj) {
         livros.put(obj.getISBN(), obj);
-        addLivroEmCategoria(obj);
-        addLivroEmAutor(obj);
         return obj;
     }
 
@@ -82,102 +81,25 @@ public class LivroDAOList implements LivroDAO{
         return livrosTitulo;
     }
 
-    @Override
-    public void addCategoria(String categoria) {
-        LinkedList<String> ISBNs = new LinkedList<String>();
-        isbnPorCategorias.put(categoria, ISBNs);
-    }
 
-    @Override
-    public Livro addLivroEmCategoria(Livro obj) {
-        String categoria = obj.getCategoria();
-
-        if(isbnPorCategorias.get(categoria) != null)
-            isbnPorCategorias.get(categoria).add(obj.getISBN());
-        else {
-            LinkedList<String> isbns = new LinkedList<>();
-            isbns.add(obj.getISBN());
-            isbnPorCategorias.put(categoria, isbns);
-        }
-
-        return obj;
-    }
-    @Override
-    public void removerLivroDeCategoria(Livro obj, String categoria) {
-        LinkedList<String> isbns = isbnPorCategorias.get(categoria);
-        if(isbns != null) {
-            for (String i : isbns) {
-                if (i.equals(obj.getISBN())) {
-                    int index = isbns.indexOf(i);
-                    if (index != -1) {
-                        isbns.remove(index);
-                        return;
-                    }
-                }
-            }
-            isbnPorCategorias.put(categoria, isbns);
-        }
-
-    }
 
     @Override
     public List<Livro> findByCategoria(String categoria) {
-        LinkedList<String> ISBNsDaCategoria = isbnPorCategorias.get(categoria);
-        List<Livro> livrosDaCategoria = new LinkedList<Livro>();
-        for(String isbn: ISBNsDaCategoria){
-            Livro livro = livros.get(isbn);
-            livrosDaCategoria.add(livro);
-        }
-
-        return livrosDaCategoria;
+        return findMany()
+                .stream()
+                .filter(l -> l.getCategoria().equals(categoria))
+                .collect(Collectors.toList());
     }
 
-    @Override
-    public void addAutor(String autor) {
-        LinkedList<String> ISBNs = new LinkedList<String>();
-        isbnPorAutores.put(autor, ISBNs);
-    }
 
     @Override
     public List<Livro> findByAutor(String autor) {
-        LinkedList<String> ISBNsDoAutor = isbnPorAutores.get(autor);
-        List<Livro> livrosDoAutor = new LinkedList<Livro>();
-        for(String isbn: ISBNsDoAutor){
-            Livro livro = livros.get(isbn);
-            livrosDoAutor.add(livro);
-        }
-        return livrosDoAutor;
+        return findMany()
+                .stream()
+                .filter(l -> l.getAutor().equals(autor))
+                .collect(Collectors.toList());
     }
 
-    @Override
-    public Livro addLivroEmAutor(Livro obj) {
-        String autor = obj.getAutor();
-
-        if(isbnPorAutores.get(autor) != null)
-            isbnPorAutores.get(autor).add(obj.getISBN());
-        else {
-            LinkedList<String> isbns = new LinkedList<>();
-            isbns.add(obj.getISBN());
-            isbnPorAutores.put(autor, isbns);
-        }
-
-        return obj;
-    }
-
-    @Override
-    public void removerLivroDeAutor(Livro obj, String autor) {
-        LinkedList<String> isbns = isbnPorAutores.get(autor);
-        if(isbns != null) {
-            for (String i : isbns) {
-                if (i.equals(obj.getISBN())) {
-                    int index = isbns.indexOf(i);
-                    if (index != -1)
-                        isbns.remove(index);
-                }
-            }
-            isbnPorAutores.put(autor, isbns);
-        }
-    }
 
 
 }
