6f64e0e0dbd11f6bc9c4a79f72e0a0a79b459c3f
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-11-26 14:38:14 -0300

    :sparkles: feat: nova mensagem de exception

--- src/main/java/com/uefs/librarymanager/exceptions/EmprestimoException.java
+++ src/main/java/com/uefs/librarymanager/exceptions/EmprestimoException.java
@@ -4,6 +4,7 @@ package com.uefs.librarymanager.exceptions;
 
 public class EmprestimoException extends Exception {
     public static final String LIMITE_RENOVACOES = "Limite de reservas excedido.";
+    public static final String USUARIO_AINDA_TEM_EMPRESTIMOS = "Usuario ainda tem empréstimos em andamento";
     public EmprestimoException(String message) {
         super(message);
     }
