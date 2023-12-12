68ae539031248c4c2be870fc8be1bf5e34b4aef4
kevincorges <141889047+kevincorges@users.noreply.github.com>
2023-12-11 21:48:50 -0300

    :books: feat Criação tela de login

--- src/main/java/com/uefs/librarymanager/controller/HelloController.java
+++ src/main/java/com/uefs/librarymanager/controller/HelloController.java
@@ -1,14 +1,52 @@
 package com.uefs.librarymanager.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField IDacesso;
+
+    @FXML
+    private TextField Senha;
+
+    @FXML
+    private RadioButton btnAdm;
+
+    @FXML
+    private RadioButton btnBibliotecario;
+
+    @FXML
+    private RadioButton btnConvidado;
+
+    @FXML
+    private RadioButton btnLeitor;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    void btnAdmAction(ActionEvent event) {
+
     }
+
+    @FXML
+    void btnBibliotecarioAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnConvidadoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoginAction(ActionEvent event) {
+
+    }
+
 }
