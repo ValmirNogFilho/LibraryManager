08a287285cf7e6a8176016f39b85b9cdc403c1c2
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-12-26 14:56:51 -0300

    :sparkles: feat: add redirecionamento de janela após login

--- src/main/java/com/uefs/librarymanager/controller/HelloController.java
+++ src/main/java/com/uefs/librarymanager/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.uefs.librarymanager.controller;
 
+import com.uefs.librarymanager.HelloApplication;
 import com.uefs.librarymanager.dao.DAO;
 import com.uefs.librarymanager.exceptions.UsuarioException;
 import com.uefs.librarymanager.model.Leitor;
@@ -7,9 +8,15 @@ import com.uefs.librarymanager.model.Usuario;
 import com.uefs.librarymanager.utils.cargoUsuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class HelloController {
 
@@ -61,10 +68,19 @@ public class HelloController {
         Usuario obj = null;
         String senha = Senha.getText();
         String id = IDacesso.getText();
+        String userHomeScreen = "";
 
         switch (cargo){
-            case LEITOR -> obj = DAO.getLeitorDAO().findById(id);
-            case BIBLIOTECARIO, ADMINISTRADOR -> obj = DAO.getOperadorDAO().findById(id);
+            case LEITOR -> {
+                obj = DAO.getLeitorDAO().findById(id);
+            }
+            case BIBLIOTECARIO -> {
+                obj = DAO.getOperadorDAO().findById(id);
+            }
+            case ADMINISTRADOR -> {
+                obj = DAO.getOperadorDAO().findById(id);
+                userHomeScreen = "users-list-view.fxml";
+            }
             default -> System.out.println("entrou como convidado");
         }
 
@@ -78,8 +94,27 @@ public class HelloController {
             throw new UsuarioException(UsuarioException.SENHA_INVALIDA);
         }
 
-        System.out.println(obj);
 
+        redirectHomePage(event, userHomeScreen);
+
+    }
+
+    private void redirectHomePage(ActionEvent event, String userHomeScreen) {
+        try {
+            Stage currentScreen = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentScreen.close();
+
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource(userHomeScreen));
+            Parent root = loader.load();
+            Stage loginStage = new Stage();
+            Scene scene = new Scene(root);
+            loginStage.setResizable(false);
+            loginStage.setScene(scene);
+            loginStage.show();
+
+        } catch (Exception excep) {
+            excep.printStackTrace();
+        }
     }
 
 
