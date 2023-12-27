093ad3c32a061a675b0c10f3fa1fd53d7df57e76
ValmirNogFilho <valmirnogueira514@gmail.com>
2023-12-27 01:30:36 -0300

    :sparkles: feat: adicionando mensagem de erro e seleção única de cargo

--- src/main/java/com/uefs/librarymanager/controller/HelloController.java
+++ src/main/java/com/uefs/librarymanager/controller/HelloController.java
@@ -4,21 +4,27 @@ import com.uefs.librarymanager.HelloApplication;
 import com.uefs.librarymanager.dao.DAO;
 import com.uefs.librarymanager.exceptions.UsuarioException;
 import com.uefs.librarymanager.model.Leitor;
+import com.uefs.librarymanager.model.Sistema;
 import com.uefs.librarymanager.model.Usuario;
 import com.uefs.librarymanager.utils.cargoUsuario;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     private static cargoUsuario cargo = cargoUsuario.CONVIDADO;
     @FXML
@@ -42,6 +48,13 @@ public class HelloController {
     @FXML
     private Button btnLogin;
 
+    ToggleGroup toggleGroup;
+
+    @FXML
+    private Label warningText;
+
+    private String screenToBeRendered;
+
     @FXML
     void btnAdmAction(ActionEvent event) {
         cargo = cargoUsuario.ADMINISTRADOR;
@@ -64,47 +77,74 @@ public class HelloController {
     }
 
     @FXML
-    void btnLoginAction(ActionEvent event) throws UsuarioException {
+    void btnLoginAction(ActionEvent event) {
         Usuario obj = null;
         String senha = Senha.getText();
         String id = IDacesso.getText();
-        String userHomeScreen = "";
 
-        switch (cargo){
-            case LEITOR -> {
-                obj = DAO.getLeitorDAO().findById(id);
-            }
-            case BIBLIOTECARIO -> {
-                obj = DAO.getOperadorDAO().findById(id);
-            }
-            case ADMINISTRADOR -> {
-                obj = DAO.getOperadorDAO().findById(id);
-                userHomeScreen = "users-list-view.fxml";
-            }
-            default -> System.out.println("entrou como convidado");
+        try {
+            obj = login(id, senha);
+            redirectHomePage(event);
+        } catch (UsuarioException e) {
+            warningText.setText(e.getMessage());
         }
-
-        if(obj == null) return;
-
-        if(!cargo.equals(obj.getCargo())){
-            throw new UsuarioException(UsuarioException.NAO_EXISTENTE);
-        }
-
-        if(!obj.getSenha().equals(senha)){
-            throw new UsuarioException(UsuarioException.SENHA_INVALIDA);
-        }
-
-
-        redirectHomePage(event, userHomeScreen);
-
     }
 
-    private void redirectHomePage(ActionEvent event, String userHomeScreen) {
+    /**
+     * Este método é responsável pela aba do login no sistema. Nele é feita uma verificação para identificar se o
+     * usuário é administrador, bibliotecário ou leitor. Ao usuário ser identificado a partir do seu ID, é feita uma
+     * verificação de senha, caso a senha esteja incorreta, a exceção  SENHA_INVALIDA é ativada informando ao usuário
+     * que a senha dele difere da senha feita no cadastro.
+     *
+     * @param id
+     * @param senha
+     * @throws UsuarioException
+     */
+    private Usuario login(String id, String senha) throws UsuarioException {
+        Usuario obj = null;
+
+        if(id.length() == 0 || senha.length() == 0)
+            throw new UsuarioException(UsuarioException.NAO_EXISTENTE);
+
+        obj = filterByOccupation(id);
+
+        if(obj == null) return null;
+
+        if(!cargo.equals(obj.getCargo()))
+            throw new UsuarioException(UsuarioException.NAO_EXISTENTE);
+
+        if(!obj.getSenha().equals(senha))
+            throw new UsuarioException(UsuarioException.SENHA_INVALIDA);
+
+        return obj;
+    }
+
+    private Usuario filterByOccupation(String id) throws UsuarioException {
+        switch (cargo){
+            case LEITOR -> {
+                screenToBeRendered = "users-list-view.fxml";
+                return DAO.getLeitorDAO().findById(id);
+            }
+            case BIBLIOTECARIO -> {
+                screenToBeRendered = "users-list-view.fxml";
+                return DAO.getOperadorDAO().findById(id);
+            }
+            case ADMINISTRADOR -> {
+                screenToBeRendered = "users-list-view.fxml";
+                return DAO.getOperadorDAO().findById(id);
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    private void redirectHomePage(ActionEvent event) {
         try {
             Stage currentScreen = (Stage) ((Node) event.getSource()).getScene().getWindow();
             currentScreen.close();
 
-            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource(userHomeScreen));
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource(screenToBeRendered));
             Parent root = loader.load();
             Stage loginStage = new Stage();
             Scene scene = new Scene(root);
@@ -112,10 +152,18 @@ public class HelloController {
             loginStage.setScene(scene);
             loginStage.show();
 
-        } catch (Exception excep) {
-            excep.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        toggleGroup = new ToggleGroup();
+        ObservableList toggles = FXCollections.observableArrayList(btnAdm, btnBibliotecario,
+                btnConvidado, btnLeitor);
+        toggleGroup.getToggles().addAll(toggles);
+        toggleGroup.selectToggle(btnConvidado);
+    }
 }
