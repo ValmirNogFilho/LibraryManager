c68f2e64d5c71a07990b53031a4abcacaccd3b30
ValmirNogFilho <valmirnogueira514@gmail.com>
2024-02-05 21:26:00 -0300

    :recycle: refactor: books -> book

---
+++ src/main/java/com/uefs/librarymanager/controller/BookController.java
@@ -0,0 +1,132 @@
+package com.uefs.librarymanager.controller;
+
+import com.uefs.librarymanager.dao.DAO;
+import com.uefs.librarymanager.exceptions.LivroException;
+import com.uefs.librarymanager.exceptions.UsuarioException;
+import com.uefs.librarymanager.model.Leitor;
+import com.uefs.librarymanager.model.Livro;
+import com.uefs.librarymanager.model.Reserva;
+import com.uefs.librarymanager.utils.Session;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class BookController {
+
+    @FXML
+    private Label autorLivro;
+
+    @FXML
+    private Button btnReserva;
+
+    @FXML
+    private Label editoraLivro;
+
+    @FXML
+    private ImageView imagemLivro;
+
+    @FXML
+    private Label isbnLivro;
+
+    @FXML
+    private Label localizazaoLivro;
+
+    @FXML
+    private Label qntdDisponivel;
+
+    @FXML
+    private TextArea sinopseLivro;
+
+    private Livro book;
+
+    private Leitor user;
+
+    private boolean isReserved;
+
+    @FXML
+    void reservaAction(ActionEvent event) {
+        if(!isReserved){
+            reservarLivro();
+            isReserved = true;
+        }
+        else {
+            cancelarReserva();
+            isReserved = false;
+        }
+        updateBtnText();
+    }
+
+
+    private void reservarLivro() {
+        try {
+            DAO.getReservaDAO().registrarReserva(user, book);
+            openDialog("Sucesso!",
+        "Você foi adicionado a fila de reservistas do livro " + book.getTitulo() +  " com sucesso");
+        } catch (UsuarioException | LivroException e) {
+            openDialog("Erro!", e.getMessage());
+        }
+    }
+
+    private void cancelarReserva() {
+        DAO.getReservaDAO().cancelarReserva(user, book);
+        openDialog("Sucesso!", "Reserva do livro " + book.getTitulo() +" cancelada com sucesso");
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+    public void setBookAndRenderPage(Livro book) {
+        this.book = book;
+        user = (Leitor) Session.getUserInSession();
+        setBookInformation(book);
+        isReserved = alreadyReservedByUser(book);
+        updateBtnText();
+        imagemLivro.setImage(new Image(getClass().getResource(book.getImagemUrl()).toExternalForm()));
+        sinopseLivro.setText(book.getSinopse());
+    }
+
+
+    private boolean alreadyReservedByUser(Livro book) {
+        Reserva reserva = DAO.getReservaDAO().findByPrimaryKey(book.getISBN());
+        if (reserva == null)
+            return false;
+
+        return reserva.getIdUsuario().equals(user.getId());
+    }
+
+    private void updateBtnText() {
+        if (isReserved) {
+            btnReserva.setText("Cancelar reserva");
+        } else {
+            btnReserva.setText("Reservar");
+        }
+    }
+
+
+    private void setBookInformation(Livro book) {
+        autorLivro.setText("Autor:" + book.getAutor());
+        editoraLivro.setText("Editora: " + book.getEditora());
+        localizazaoLivro.setText("Localização: " + book.getLocalizacao());
+        isbnLivro.setText("ISBN:" + book.getISBN());
+        qntdDisponivel.setText(
+                "Exemplares disponíveis: " + book.getDisponiveis()
+        );
+
+    }
+
+    public void openDialog(String header, String message){
+        Alert warningDialog = new Alert(Alert.AlertType.WARNING);
+        warningDialog.setTitle(header);
+        warningDialog.setHeaderText(header);
+        warningDialog.setContentText(message);
+
+        warningDialog.show();
+    }
+}
