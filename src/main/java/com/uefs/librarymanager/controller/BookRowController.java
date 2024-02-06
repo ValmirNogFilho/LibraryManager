a7d632bdf7beaef786d40d85d704c076e338acaf
ValmirNogFilho <valmirnogueira514@gmail.com>
2024-02-05 21:27:31 -0300

    :sparkles: feat: add funcionalidade de abertuda da página do livro

--- src/main/java/com/uefs/librarymanager/controller/BookRowController.java
+++ src/main/java/com/uefs/librarymanager/controller/BookRowController.java
@@ -1,13 +1,20 @@
 package com.uefs.librarymanager.controller;
 
+import com.uefs.librarymanager.HelloApplication;
+import com.uefs.librarymanager.model.Livro;
+import com.uefs.librarymanager.model.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class BookRowController {
 
@@ -17,14 +24,18 @@ public class BookRowController {
     @FXML
     private Button openBookPage;
 
-    public void setTitle (String title) {
-        this.title.setText(title);
+    private Livro book;
+
+    public void setBook(Livro book) {
+        this.book = book;
+        this.title.setText(book.getTitulo());
     }
+
 
 
     @FXML
     void btnClicked(ActionEvent event) {
-
+        openBook(book);
     }
 
     @FXML
@@ -37,4 +48,21 @@ public class BookRowController {
 
     }
 
+    private void openBook(Livro book){
+        try{
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("book-view.fxml"));
+            Parent profileView = loader.load();
+            BookController bookCtrl = loader.getController();
+
+            bookCtrl.setBookAndRenderPage(book);
+            Stage profileStage = new Stage();
+            Scene scene = new Scene(profileView);
+            profileStage.setResizable(false);
+            profileStage.setScene(scene);
+            profileStage.show();
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
