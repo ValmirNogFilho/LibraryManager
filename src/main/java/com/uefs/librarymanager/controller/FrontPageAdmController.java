ca994f7e44a7fb538b78d2160ede5d7b9c634dbe
ValmirNogFilho <valmirnogueira514@gmail.com>
2024-02-07 16:48:50 -0300

    :sparkles: feat: add logout

--- src/main/java/com/uefs/librarymanager/controller/FrontPageAdmController.java
+++ src/main/java/com/uefs/librarymanager/controller/FrontPageAdmController.java
@@ -7,10 +7,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -45,7 +50,21 @@ public class FrontPageAdmController implements Initializable {
 
     @FXML
     void actionSairAdm(ActionEvent event) {
+        if (!(wantsToLogout()))
+            return;
 
+        openLoginPage(event);
+    }
+
+    private boolean wantsToLogout() {
+        Alert confirmationDialog = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationDialog.setTitle("Sair");
+        confirmationDialog.setHeaderText("Deseja sair?");
+        confirmationDialog.setContentText("Escolha 'OK' para sair");
+
+        confirmationDialog.showAndWait();
+
+        return confirmationDialog.getResult() == ButtonType.OK;
     }
 
 
@@ -54,6 +73,24 @@ public class FrontPageAdmController implements Initializable {
         adm = (Administrador) Session.getUserInSession();
         openPage("users-list-view.fxml");
         admName.setText("Bem vinda(o)," + adm.getNome());
+    }
+
+    private void openLoginPage(ActionEvent event) {
+        Session.logoutUser();
+        Stage currentScreen = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentScreen.close();
+
+        try {
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            Parent root = loader.load();
+            Stage loginStage = new Stage();
+            Scene scene = new Scene(root);
+            loginStage.setResizable(false);
+            loginStage.setScene(scene);
+            loginStage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
