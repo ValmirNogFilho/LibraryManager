9cfe1defcfef8f556ce021bffe0b11642c94a9ff
kevincorges <141889047+kevincorges@users.noreply.github.com>
2024-02-14 17:44:58 -0300

    :books: feat Criação fxml criação de usuario, cadastro de livro e fxml bibliotecario

---
+++ src/main/java/com/uefs/librarymanager/controller/CadastroUserController.java
@@ -0,0 +1,66 @@
+package com.uefs.librarymanager.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class CadastroUserController {
+
+    @FXML
+    private ImageView FotoUsuario;
+
+    @FXML
+    private RadioButton btnGerarId;
+
+    @FXML
+    private MenuButton btnTipoUsuario;
+
+    @FXML
+    private ImageView capaUsuario;
+
+    @FXML
+    private TextField cxEmail;
+
+    @FXML
+    private TextField cxEndereço;
+
+    @FXML
+    private TextField cxNomeUsuario;
+
+    @FXML
+    private TextField cxTelefone;
+
+    @FXML
+    void acionBtnTipoUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionBtnGerarId(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxEndereço(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxNomeUsusario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxTelefone(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actioncxEmail(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/uefs/librarymanager/controller/GerenciaLivrosController.java
@@ -0,0 +1,68 @@
+package com.uefs.librarymanager.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+public class GerenciaLivrosController {
+
+    @FXML
+    private Button btnBusca;
+
+    @FXML
+    private MenuButton btnOpcoes;
+
+    @FXML
+    private TextField caixaNomeLivro;
+
+    @FXML
+    private MenuItem opcaoAlterar;
+
+    @FXML
+    private MenuItem opcaoExibirReservas;
+
+    @FXML
+    private MenuItem opcaoRegistroEmprestimo;
+
+    @FXML
+    private MenuItem opcaoRemover;
+
+    @FXML
+    void actionBtnBusca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCaixaNomeLivro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionOpcaoAlterar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionOpcaoExibirReservas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionOpcaoRegistarEmprestimo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionOpcaoRemover(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionOpcoes(ActionEvent event) {
+
+    }
+
+}
