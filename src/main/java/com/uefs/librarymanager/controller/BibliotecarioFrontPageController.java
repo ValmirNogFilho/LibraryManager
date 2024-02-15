2debb3587440f9667c349744ef12b9ee66c5fcc0
kevincorges <141889047+kevincorges@users.noreply.github.com>
2024-02-15 12:48:56 -0300

    :books: feat Criação fxml cadastro usuário, cadastro de livro e pagina inicial bibliotecario

---
+++ src/main/java/com/uefs/librarymanager/controller/BibliotecarioFrontPageController.java
@@ -0,0 +1,45 @@
+package com.uefs.librarymanager.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class BibliotecarioFrontPageController {
+
+    @FXML
+    private Button btnCadastrarLivros;
+
+    @FXML
+    private Button btnInicio;
+
+    @FXML
+    private Button btnLivros;
+
+    @FXML
+    private Button btnSair;
+
+    @FXML
+    private BorderPane painelPrincipal;
+
+    @FXML
+    void actionCadastrarLivros(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inicioAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void livroAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sairAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/uefs/librarymanager/controller/CadastroLivroController.java
@@ -0,0 +1,75 @@
+package com.uefs.librarymanager.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class CadastroLivroController {
+
+    @FXML
+    private ImageView capaLivro;
+
+    @FXML
+    private TextField cxISBN;
+
+    @FXML
+    private TextField cxLocalizacao;
+
+    @FXML
+    private TextField cxNomeAutor;
+
+    @FXML
+    private TextField cxNomeCategoria;
+
+    @FXML
+    private TextField cxNomeEditora;
+
+    @FXML
+    private TextField cxNomeLivro;
+
+    @FXML
+    private Label cxQuantidade;
+
+    @FXML
+    private TextArea sinopseLivro;
+
+    @FXML
+    void actionCxISBN(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxLocalizacao(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxNomeAutor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxNomeCategoria(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxNomeEditora(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxNomeLivro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCxQuantidade(MouseEvent event) {
+
+    }
+
+}
