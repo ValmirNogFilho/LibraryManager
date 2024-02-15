222c73bcc38340180dc27149e4025855054d14ee
ValmirNogFilho <valmirnogueira514@gmail.com>
2024-02-15 14:33:07 -0300

    :sparkles: feat: impl cadastro de livros

--- src/main/java/com/uefs/librarymanager/controller/CadastroLivroController.java
+++ src/main/java/com/uefs/librarymanager/controller/CadastroLivroController.java
@@ -1,14 +1,25 @@
 package com.uefs.librarymanager.controller;
 
+import com.uefs.librarymanager.dao.DAO;
+import com.uefs.librarymanager.model.Livro;
+import com.uefs.librarymanager.model.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
 
-public class CadastroLivroController {
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class CadastroLivroController implements Initializable {
 
     @FXML
     private ImageView capaLivro;
@@ -32,44 +43,96 @@ public class CadastroLivroController {
     private TextField cxNomeLivro;
 
     @FXML
-    private Label cxQuantidade;
+    private TextField cxQuantidade;
+
+    @FXML
+    private TextField cxAnoPublicacao;
 
     @FXML
     private TextArea sinopseLivro;
 
     @FXML
-    void actionCxISBN(ActionEvent event) {
+    private Button registerBtn;
 
+    private String defaultCoverUrl = getClass().getResource("/img/book-covers/template.jpg").toExternalForm();
+    @FXML
+    void openFile(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        File selected = fileChooser.showOpenDialog(HelloController.mainPage);
+        try {
+            File copy = copy(selected);
+            capaLivro.setImage(new Image(copy.getAbsolutePath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
-    void actionCxLocalizacao(ActionEvent event) {
+    void registerAction(ActionEvent event) {
+        TextField missingDataTextField = assertMissingTextFields();
+
+        if(missingDataTextField != null){
+            missingDataAlert(missingDataTextField.getId());
+            return;
+        }
+
+        Livro book;
+
+        if(sinopseLivro.getText().isEmpty() || capaLivro.getImage().getUrl().equals(
+                getClass().getResource("/img/book-covers/template.jpg").toExternalForm()
+        ) || capaLivro.getImage().getUrl().equals(defaultCoverUrl))
+            book = new Livro(cxNomeLivro.getText(), cxNomeAutor.getText(), cxNomeEditora.getText(), cxISBN.getText(),
+                        Integer.parseInt(cxAnoPublicacao.getText()), cxLocalizacao.getText(), cxNomeCategoria.getText(),
+                    Integer.parseInt(cxQuantidade.getText()));
+        else
+            book = new Livro(cxNomeLivro.getText(), cxNomeAutor.getText(), cxNomeEditora.getText(), cxISBN.getText(),
+                    Integer.parseInt(cxAnoPublicacao.getText()), cxLocalizacao.getText(), cxNomeCategoria.getText(),
+                    Integer.parseInt(cxQuantidade.getText()), sinopseLivro.getText(), capaLivro.getImage().getUrl());
+
+        DAO.getLivroDAO().create(book);
+        alert("Operação concluída!", "Operação concluída!", "Livro" + book.getTitulo() + "cadastrado com sucesso!");
 
     }
 
-    @FXML
-    void actionCxNomeAutor(ActionEvent event) {
+    private TextField assertMissingTextFields() {
+        if(cxNomeLivro.getText().isEmpty()) return cxNomeLivro;
+        if(cxNomeAutor.getText().isEmpty()) return cxNomeAutor;
+        if(cxISBN.getText().isEmpty()) return cxISBN;
+        if(cxNomeEditora.getText().isEmpty()) return cxNomeEditora;
+        if(cxNomeCategoria.getText().isEmpty()) return cxNomeCategoria;
+        if(cxLocalizacao.getText().isEmpty()) return cxLocalizacao;
+        if(cxQuantidade.getText().isEmpty()) return cxQuantidade;
+        if(cxAnoPublicacao.getText().isEmpty()) return cxAnoPublicacao;
+        return null;
 
     }
 
-    @FXML
-    void actionCxNomeCategoria(ActionEvent event) {
-
+    private void missingDataAlert(String componentName) {
+        alert("Dados obrigatórios faltando!", "Dados obrigatórios faltando!",
+                componentName + "está vazio. Por favor, preencha esse dado");
     }
 
-    @FXML
-    void actionCxNomeEditora(ActionEvent event) {
+    private void alert(String title, String header, String content) {
+        Alert confirmationDialog = new Alert(Alert.AlertType.WARNING);
+        confirmationDialog.setTitle(title);
+        confirmationDialog.setHeaderText(header);
+        confirmationDialog.setContentText(content);
 
+        confirmationDialog.show();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        capaLivro.setImage(new Image(
+                defaultCoverUrl
+        ));
     }
 
-    @FXML
-    void actionCxNomeLivro(ActionEvent event) {
+    private File copy(File file) throws IOException {
+        File src = new File(file.getPath());
+        File dest = new File(getClass().getResource("/img/book-covers/") + file.getName());
 
+        Files.copy(src.toPath(), dest.toPath());
+        return dest;
     }
-
-    @FXML
-    void actionCxQuantidade(MouseEvent event) {
-
-    }
-
 }
