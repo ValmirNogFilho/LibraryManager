c2c29696bcac06956989e8d4c865bdc5a7f91a2c
ValmirNogFilho <valmirnogueira514@gmail.com>
2024-02-16 15:15:47 -0300

    :sparkles: feat: atualização e cadastro de livros funcionando parcialmente (erro em loading da imagem)

--- src/main/java/com/uefs/librarymanager/controller/AtualizacaoLivrosController.java
+++ src/main/java/com/uefs/librarymanager/controller/AtualizacaoLivrosController.java
@@ -1,30 +1,33 @@
 package com.uefs.librarymanager.controller;
 
+import com.uefs.librarymanager.HelloApplication;
 import com.uefs.librarymanager.dao.DAO;
 import com.uefs.librarymanager.model.Livro;
-import com.uefs.librarymanager.utils.FileUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
 import java.util.ResourceBundle;
 
-public class CadastroLivroController implements Initializable {
+public class AtualizacaoLivrosController implements Initializable {
 
     @FXML
     private ImageView capaLivro;
+
+    @FXML
+    private TextField cxAnoPublicacao;
 
     @FXML
     private TextField cxISBN;
@@ -48,58 +51,45 @@ public class CadastroLivroController implements Initializable {
     private TextField cxQuantidade;
 
     @FXML
-    private TextField cxAnoPublicacao;
+    private Button registerBtn;
 
     @FXML
     private TextArea sinopseLivro;
 
-    @FXML
-    private Button registerBtn;
-
-    private TextField[] allTextFields;
-
-    private String selectedImageUrl;
+    private Livro book;
 
     private File selected;
 
+    private String selectedImageUrl;
+
+    private TextField[] allTextFields;
+
     private String defaultCoverUrl = getClass().getResource("/img/book-covers/template.jpg").toExternalForm();
+
     @FXML
     void openFile(ActionEvent event) throws MalformedURLException {
         FileChooser fileChooser = new FileChooser();
         selected = fileChooser.showOpenDialog(HelloController.mainPage);
         if(selected != null) {
-            String selectedImageUrl = selected.toURI().toURL().toString();
+             selectedImageUrl = selected.toURI().toURL().toString();
             capaLivro.setImage(new Image(selectedImageUrl));
         }
     }
 
     @FXML
-    void registerAction(ActionEvent event) {
+    void updateAction(ActionEvent event) {
         TextField missingDataTextField = assertMissingTextFields();
 
         if(missingDataTextField != null){
             missingDataAlert(missingDataTextField.getPromptText());
             return;
         }
+        getBookData();
 
-        Livro book;
-
-        if(sinopseLivro.getText().isEmpty() || selectedImageUrl == null ||
-                selectedImageUrl.equals(defaultCoverUrl))
-            book = new Livro(cxNomeLivro.getText(), cxNomeAutor.getText(), cxNomeEditora.getText(), cxISBN.getText(),
-                        Integer.parseInt(cxAnoPublicacao.getText()), cxLocalizacao.getText(), cxNomeCategoria.getText(),
-                    Integer.parseInt(cxQuantidade.getText()));
-        else{
-            book = new Livro(cxNomeLivro.getText(), cxNomeAutor.getText(), cxNomeEditora.getText(), cxISBN.getText(),
-                    Integer.parseInt(cxAnoPublicacao.getText()), cxLocalizacao.getText(), cxNomeCategoria.getText(),
-                    Integer.parseInt(cxQuantidade.getText()), sinopseLivro.getText(), capaLivro.getImage().getUrl());
-
-        }
-//        FileUtils.copiarImagemParaBookCovers(selected); TODO
-
-        DAO.getLivroDAO().create(book);
-        alert("Operação concluída!", "Operação concluída!", "Livro " + book.getTitulo() + " cadastrado com sucesso!");
-        blankAllFields();
+        DAO.getLivroDAO().update(book);
+        alert("Operação concluída!", "Operação concluída!", "Livro " + book.getTitulo() + " atualizado com sucesso!");
+        Stage currentScreen = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentScreen.close();
     }
 
     private TextField assertMissingTextFields() {
@@ -154,5 +144,38 @@ public class CadastroLivroController implements Initializable {
 
     }
 
+    public void setBookAndRender(Livro book) {
+        this.book = book;
+        setBookData();
+    }
+
+    private void setBookData() {
+        cxNomeLivro.setText(book.getTitulo());
+        cxISBN.setText(book.getISBN());
+        cxNomeCategoria.setText(book.getCategoria());
+        cxNomeAutor.setText(book.getAutor());
+        cxNomeEditora.setText(book.getEditora());
+        cxLocalizacao.setText(book.getLocalizacao());
+        cxQuantidade.setText(String.valueOf(book.getDisponiveis()));
+        sinopseLivro.setText(book.getSinopse());
+        capaLivro.setImage(new Image(
+                        HelloApplication.class.getResource(book.getImagemUrl()).toExternalForm()
+                )
+        ); //TODO
+        cxISBN.setText(String.valueOf(book.getDisponiveis()));
+    }
+
+    private void getBookData() {
+        book.setTitulo(cxNomeLivro.getText());
+        book.setAutor(cxNomeAutor.getText());
+        book.setCategoria(cxNomeCategoria.getText());
+        book.setEditora(cxNomeEditora.getText());
+        book.setDisponiveis(Integer.parseInt(cxQuantidade.getText()));
+        book.setAnoDePublicacao(cxAnoPublicacao.getText().isEmpty()? 0 :  Integer.parseInt(cxAnoPublicacao.getText()));
+        book.setImagemUrl(capaLivro.getImage().getUrl()); // TODO
+        book.setISBN(cxISBN.getText());
+        book.setLocalizacao(cxLocalizacao.getText());
+        book.setSinopse(sinopseLivro.getText());
+    }
 
 }
